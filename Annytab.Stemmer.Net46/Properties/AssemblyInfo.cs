0bda18328b4cbd0f82726525e5db7f8355d9687e
alexander_zhoshkin <alexander_zhoshkin@epam.com>
2017-11-13 08:38:24 +0300

    .Net 4.6.1

---
+++ Annytab.Stemmer.Net46/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Annytab.Stemmer")]
+[assembly: AssemblyDescription("Stemmers for 12 languages and one default stemmer. The default stemmer (DefaultStemmer) can be used for languages that not are supported. The default stemmer does not do any stemming at all.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("A Name Not Yet Taken AB")]
+[assembly: AssemblyProduct("a-stemmer")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ddfcf462-c3fc-4793-9325-c3acb08940d3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.2.3.0")]
+[assembly: AssemblyFileVersion("1.2.3.0")]
